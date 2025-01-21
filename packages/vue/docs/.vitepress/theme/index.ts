99fa5e7e385ac2cc562461d60040bade28753ffb
Jacob Sampson <jacob.samps@gmail.com>
2025-01-20 21:14:25 -0600

    Use Formik drag-and-drop

--- packages/vue/docs/.vitepress/theme/index.ts
+++ packages/vue/docs/.vitepress/theme/index.ts
@@ -3,7 +3,6 @@ import DefaultTheme from 'vitepress/theme-without-fonts';
 import PrimeVue from 'primevue/config';
 import Tooltip from 'primevue/tooltip';
 import { MotionPlugin } from '@vueuse/motion'
-import { VueDraggableNext } from 'vue-draggable-next'
 
 import Layout from './Layout.vue';
 import PlayingCard from '../../../src/ui/elements/PlayingCard.vue';
@@ -18,8 +17,6 @@ export default {
     app.use(PrimeVue, { theme: 'none'}); 
     app.use(MotionPlugin);
     app.component('PlayingCard', PlayingCard);
-    // https://github.com/SortableJS/Sortable#event-object-demo
-    app.component('draggable', VueDraggableNext);
     app.directive('tooltip', Tooltip);
   }
 } satisfies Theme;
--- packages/vue/src/ui/elements/Dropzone.vue
+++ packages/vue/src/ui/elements/Dropzone.vue
@@ -1,10 +1,14 @@
 <script lang="ts" setup>
-import { computed, defineProps, ref } from 'vue';
+import { Ref } from 'vue';
+import { computed, watch, defineProps, ref } from 'vue';
+import { VueDraggable } from 'vue-draggable-plus';
+import { useElementBounding } from '@vueuse/core';
 
-const { shape, dimensions, component } = defineProps<{
+const { shape, dimensions, component, label } = defineProps<{
   shape: number | [number, number];
   dimensions: [string | number, string | number];
   component: string;
+  label?: string;
 }>();
 
 // Default to x by 1 grid
@@ -18,59 +22,75 @@ const height = computed(() =>
   typeof dimensions[1] === 'number' ? `${dimensions[1]}px` : dimensions[1],
 );
 
-const componentProps = ref([]);
-const full = computed(() => componentProps.value.length === n.value * m.value);
-
-const onDrop = computed(() => (event: DragEvent) => {
-  dragHovered.value = false;
-  event.dataTransfer.dropEffect = full.value ? 'none' : 'move';
-
-  if (full.value) {
-    return;
-  }
-
-  const props = JSON.parse(event.dataTransfer.getData('props'));
-  componentProps.value.push(props);
-});
-
 const dragHovered = ref(false);
-
-const elements = computed(() =>
-  Array.from({ length: n.value * m.value }, (_, index) => ({
-    index,
-    props: componentProps.value[index],
-  })),
-);
 
 const onDragOver = computed(() => (event: DragEvent) => {
   dragHovered.value = true;
-  event.dataTransfer.dropEffect = full.value ? 'none' : 'move';
+});
+
+const elements = ref(
+  Array.from({ length: n.value * m.value }).map(() => null as unknown),
+) as Ref<object[]>;
+
+const full = computed(() => elements.value.length >= n.value * m.value * 2);
+
+const draggable = ref(null);
+const { top, right, bottom, left } = useElementBounding(draggable);
+const onTouchMove = computed(() => (event: TouchEvent) => {
+  const { pageX, pageY } = event.targetTouches[0];
+  if (
+    pageX < left.value ||
+    pageX > right.value ||
+    pageY < top.value ||
+    pageY > bottom.value
+  ) {
+    dragHovered.value = false;
+  } else if (
+    pageX > left.value &&
+    pageX < right.value &&
+    pageY > top.value &&
+    pageY < bottom.value
+  ) {
+    dragHovered.value = true;
+  }
+});
+
+watch(elements, () => {
+  dragHovered.value = false;
 });
 </script>
 
 <template>
-  <div
-    class="grid gap-2"
+  <VueDraggable
+    ref="draggable"
+    v-model="elements"
+    ghost-class="hidden"
+    group="items"
+    class="grid gap-2 select-none"
+    :animation="150"
+    :delay="250"
+    :class="[
+      full && 'pointer-events-none select-none hover:cursor-not-allowed',
+    ]"
     :style="`grid-template-columns: repeat(${m}, ${width}); grid-template-rows: repeat(${height}, ${n})`"
+    @dragover="onDragOver"
+    @touchmove="onTouchMove"
+    @dragleave="dragHovered = false"
+    @dragover.prevent
+    @dragenter.prevent
   >
     <div
-      v-for="element in elements"
+      v-for="element in elements.sort().slice(0, n * m)"
       class="relative transition-all ease-in-out rounded-md bg-slate-200"
       :class="[!full && dragHovered ? 'bg-slate-300' : 'bg-slate-200']"
       :style="`width: ${width}; height: ${height}`"
-      @dragover="onDragOver"
-      @dragleave="dragHovered = false"
-      @dragover.prevent
-      @dragenter.prevent
-      @drop="onDrop($event)"
     >
       <component
-        class="absolute top-0 left-0 pointer-events-none select-non"
-        v-if="element.props"
+        class="absolute top-0 left-0 pointer-events-none"
+        v-if="element"
         :is="component"
-        v-bind="element.props"
+        v-bind="element"
       />
     </div>
-    <slot></slot>
-  </div>
+  </VueDraggable>
 </template>
